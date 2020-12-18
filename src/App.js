467989b15c5ac4d18cc549df6982f617fc649c9e
Emmanuel Kolawole <ekolawole@MBP-EKOLAWOLE.local>
2020-12-19 00:37:30 +0200

    Created HomePage component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { GlobalStyle } from "./GlobalStyle";
-import Header from "./components/Header/index";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <GlobalStyle />
     </div>
   );

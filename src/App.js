8f5b5c3efb78487681ae3dc9b991645437e899b1
Emmanuel Kolawole <ekolawole@MBP-EKOLAWOLE.local>
2020-12-18 02:15:42 +0200

    Added header component & Logos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { GlobalStyle } from "./GlobalStyle";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      Start here.
+      <Header />
       <GlobalStyle />
     </div>
   );

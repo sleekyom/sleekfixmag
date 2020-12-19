e8a0911164e738186f3d32d5d4bf1d6d22d1e1da
Emmanuel Kolawole <amdas72@gmail.com>
2020-12-19 02:07:09 +0200

    Fetched movies from API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,36 @@
-import React, {useState} from 'react';
-import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
-import NoImage from '../images/no_image.jpg';
+import React, { useState, useEffect } from "react";
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
+import NoImage from "../images/no_image.jpg";
+import API from "../API"
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setloading] = useState(false);
+	const [state, setState] = useState();
+	const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  return (
-    <div>
-    Home page
-  </div>
-  )
-}
+  const fetchMovies = async (page, searchTerm = '') => {
+    try {
+      setError(false);
+      setLoading(true);
+      const movies = await API.fetchMovies(searchTerm, page);
+
+      setState(prevState => ({
+        ...movies,
+        results:
+          page > 1 ? [...prevState.results, ...movies.results] : [...movies.results]
+      }));
+
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchMovies(1);
+  }, [])
+  console.log(state);
+  return <div>Home page</div>;
+};
 
 export default Home;
